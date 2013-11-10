65bb3b97df670a7535c70da1e3872833b454e728
Jonathan Bodian <jbodian@gmail.com>
2013-11-09 22:18:28 -0500

    views issue

--- app/views.py
+++ app/views.py
@@ -1,12 +1,12 @@
 from flask import render_template
+from forms import AddressForm
 from app import app
-#from forms import AddressForm
 
 @app.route('/')
 def home():
   return render_template('home.html')
 
-#@app.route('/order', methods = ['GET','POST'])
-#def order():
-#  form = AddressForm()
-#  return(render_template('order.html',form=form)
+@app.route('/order', methods = ['GET','POST'])
+def order():
+  form = AddressForm()
+  return(render_template('order.html',form=form)
